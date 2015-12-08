fbfa3ae0eb57b7e2e42320554b758cab77603cbf
Eliatrope <hugo.ballabriga@laposte.net>
2015-12-08 14:03:05 +0100

    Création fichier article

---
+++ article.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="description" content="$nomunivers | Bienvenue dans l'univers $nomunivers. Ici, on parle de tout, mais surtout de n'importe quoi."/><!--cf index.php-->
+		<link type="text/css" rel="stylesheet" href="asset/css/style.css" />
+		<link type="text/css" rel="stylesheet" href="asset/css/smallresponsive.css" />
+		<link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+		<title>Bienvenue dans l'univers du BALLA et d'Eli'. Ici, on parle de tout, mais surtout de n'importe quoi et ce, sans tabou</title>
+	</head>
+	
+	<body>
+		<?php 
+			include_once('asset/php/menu.php');
+		?>
+		<section>
+			
+		</section>
+		<script src="js/jquery-2.1.4.min.js"></script>
+		<script src="js/menu.js"></script>
+	</body>
+</html>
